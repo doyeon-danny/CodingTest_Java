4f3e7f1602edc089f9dee6edc81203dbb1e1685b
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2025-11-25 10:33:42 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 93.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        // int[] answer = {};
+        int[] answer = new int[num_list.length+1];
+        for (int i=0; i <= num_list.length; i++){
+            if (i == num_list.length){
+                if (num_list[i-1] > num_list[i-2]){
+                    answer[i] = num_list[i-1] - num_list[i-2];
+                } else {
+                    // answer[i] = Math.pow(num_list[i-1], 2);
+                    answer[i] = num_list[i-1] * 2;
+                }
+            } else {
+                answer[i] = num_list[i];
+            }
+        }
+        return answer;
+    }
+}
