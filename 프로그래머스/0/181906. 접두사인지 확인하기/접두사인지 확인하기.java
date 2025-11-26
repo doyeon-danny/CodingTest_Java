92f3cab7e4ab0253cd582dc035339f8caecf2082
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2025-11-26 09:00:50 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
         // return my_string.startsWith(is_prefix)? 1 : 0;
-        if (my_string.length() >= is_prefix.length()) {
-            return my_string.substring(0, is_prefix.length()).equals(is_prefix)? 1 : 0;
+        if (my_string.length() < is_prefix.length()) {
+            return 0;
         }
-        return 0;
+        return my_string.substring(0, is_prefix.length()).equals(is_prefix)? 1 : 0;
     }
 }
