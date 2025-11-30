a6b7b0c45ad4f9feca1da38d1a8554a47005e476
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2025-12-01 08:50:22 +0900

    [level 0] Title: 0 떼기, Time: 7.98 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String n_str) {
+        int cnt = 0;
+        for(int i=0; i<n_str.length(); i++) {
+            if (("" + n_str.charAt(i)).equals("0") == false) {
+                break;
+            }
+            cnt += 1;
+            // System.out.println(("" + n_str.charAt(i)).equals("0"));
+        }
+        // System.out.println(cnt);
+        return n_str.substring(cnt);
+        }
+}
