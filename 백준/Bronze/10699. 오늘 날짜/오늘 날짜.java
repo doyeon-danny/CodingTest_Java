ca74901ae040c74ff7f294527ef719ac0923790c
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2025-12-21 21:56:26 +0900

    [Bronze V] Title: 오늘 날짜, Time: 160 ms, Memory: 17268 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+import java.time.LocalDate;
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(LocalDate.now());
+	}
+
+}
