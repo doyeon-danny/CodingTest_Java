3fa7d6a3a171aa8a8c642b1e2d199ef8184f8738
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2025-12-29 14:03:04 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = 0;
+        int b = 0;
+        while(st.hasMoreTokens()) {
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
