9ebbed27f8d64046292a87d42833b16cb7dc7928
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-01-04 23:18:59 +0900

    [Bronze V] Title: 사파리월드, Time: 104 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+        System.out.println(Math.abs(m - n));
+    }
+}
