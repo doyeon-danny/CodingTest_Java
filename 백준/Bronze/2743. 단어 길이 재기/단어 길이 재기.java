e190b6669826db6701b81b0daf1fe4b84dcff7db
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-01-05 19:36:10 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = String.valueOf(br.readLine());
+        System.out.println(word.length());
+	}
+
+}
