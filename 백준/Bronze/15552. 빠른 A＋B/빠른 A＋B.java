770c47ea6eff289d8ec9ad28b6b05f93dc8d3062
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-01-13 23:33:52 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 804 ms, Memory: 250084 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b + "\n");
+        }
+        System.out.println(sb);
+    }
+}
