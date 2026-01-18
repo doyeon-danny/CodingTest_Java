26e624a33bd11c0576c626235e2a927f36d5ed34
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-01-19 00:00:17 +0900

    [Bronze V] Title: 아스키 코드, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = br.readLine().charAt(0);
+        System.out.println(x);
+    }
+}
