902ffefb19014dac2c7fdbd1987da2eef53403c4
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-02-12 15:39:03 +0900

    [level 2] Title: 기능개발, Time: 10.77 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,38 +3,28 @@ import java.io.*;
 
 public class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // int[] answer = {};
-        int len = speeds.length;
-        int[] days = new int[len];
-        ArrayDeque<Integer> dq = new ArrayDeque<>();
-        ArrayList<Integer> arr = new ArrayList<>();
 
-        for (int i = 0; i < len; i++) {
-            days[i] = (100 - progresses[i]) / speeds[i];
-            if ((100 - progresses[i]) % speeds[i] > 0) {
-                days[i]++;
+        List<Integer> result = new ArrayList<>();
+
+        int prev = 0;
+        int count = 0;
+
+        for (int i = 0; i < progresses.length; i++) {
+
+            int day = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
+
+            if (day > prev) {
+                if (count > 0) result.add(count);
+                prev = day;
+                count = 1;
+            } else {
+                count++;
             }
-            System.out.println("days[i]: " + days[i]);
         }
-        dq.addLast(days[0]);
-        
-        int temp = 1;
-        for (int i = 1; i < len; i++) {
-        	int curr = dq.peekLast();
-        	if (days[i] <= curr) {
-        		temp++;
-        	} else {
-	        	dq.addLast(days[i]);
-	        	arr.add(temp);
-	        	temp = 1;
-        	}
-        }
-        arr.add(temp);
-        
-        int[] answer = new int[arr.size()];
-        for (int i = 0; i < arr.size(); i++) {
-        	answer[i] = arr.get(i);
-        }
-        return answer;
+
+        result.add(count);
+
+        return result.stream().mapToInt(i -> i).toArray();
     }
+
 }
