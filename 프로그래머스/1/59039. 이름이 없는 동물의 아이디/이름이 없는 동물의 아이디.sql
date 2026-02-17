d20f00f361c17dc07b27bbce72b53479231eaa50
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-02-17 23:11:03 +0900

    [level 1] Title: 이름이 없는 동물의 아이디, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59039. 이름이 없는 동물의 아이디/이름이 없는 동물의 아이디.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+# SELECT animal_id from animal_ins where name is null
+# select name from animal_ins group by name
+select animal_id
+from animal_ins
+where name is null
+order by animal_id;
