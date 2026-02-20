7c3d8b6b6f0b365574073e75ee89b72f729adfcc
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-02-20 17:10:09 +0900

    [level 0] Title: 할 일 목록, Time: 0.06 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,18 +1,6 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        // String[] answer = {};
-        ArrayList<String> arr = new ArrayList<>();
-        for (int i = 0; i < todo_list.length; i++) {
-            if (finished[i] == false) {
-                arr.add(todo_list[i]);
-            }
-        }
-        String[] answer = new String[arr.size()];
-        for (int i = 0; i < arr.size(); i++) {
-            answer[i] = arr.get(i);
-        }
+        String[] answer = {};
         return answer;
     }
 }
