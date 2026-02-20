723963c8b169c266337dce43039408e71c23d073
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-02-21 08:20:55 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 25.40 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -1,13 +1,18 @@
 import java.util.*;
-
 class Solution {
     public String[] solution(String myString) {
-        // String[] answer = {};
-        StringTokenizer st = new StringTokenizer(myString, "x");        
-        String[] answer = new String[st.countTokens()];
-        int idx = 0;
-        while(st.hasMoreTokens()) {
-            answer[idx++] = st.nextToken();
+        String[] arr = myString.split("x");
+        ArrayList<String> arr2 = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (!arr[i].equals("")) {
+                arr2.add(arr[i]);
+            }
+        }
+        
+        // Arrays.sort(arr2);
+        String[] answer = new String[arr2.size()];
+        for (int i = 0 ; i < arr2.size(); i++) {
+            answer[i] = arr2.get(i);
         }
         Arrays.sort(answer);
         return answer;
