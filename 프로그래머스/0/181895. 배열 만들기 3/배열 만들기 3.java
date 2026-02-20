82bd7ba72a34c7ce8a801bee1c485d1b71f7a82b
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-02-20 10:23:09 +0900

    [level 0] Title: 배열 만들기 3, Time: 9.63 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -2,17 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        ArrayList<Integer> res = new ArrayList<>();
-        for (int i = intervals[0][0]; i <= intervals[0][1]; i++) {
-            res.add(arr[i]);
-        }
-        for (int i = intervals[1][0]; i <= intervals[1][1]; i++) {
-            res.add(arr[i]);
-        }
-        int[] answer = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) {
-            answer[i] = res.get(i);
-        }
+        int[] answer = {};
+        System.out.println(arr.substring(intervals[0][0], intervals[0][1]));
         return answer;
     }
 }
