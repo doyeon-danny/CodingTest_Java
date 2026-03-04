9e21c9160ef7decee385c5f48bf006fbf16dd63c
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-03-04 22:06:15 +0900

    [level 2] Title: 올바른 괄호, Time: 6.29 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,21 +2,23 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    boolean solution(String s) {
+        boolean solution(String s) {
         boolean answer = true;
-        ArrayDeque<String> dq = new ArrayDeque<>();
-        dq.push(String.valueOf(s.charAt(0)));
-        for (int i = 1; i < s.length(); i++) {
-            String curr = String.valueOf(s.charAt(i));
-            if (!dq.isEmpty() && dq.peek().equals("(") && curr.equals(")")) {
-                dq.pop();
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                sum--;
             } else {
-                dq.push(curr);
+                sum++;
+            }
+            if (sum < 0) {
+                return false;
             }
         }
-        if (!dq.isEmpty()) {
-            answer = false;
+        if (sum != 0) {
+            return false;
         }
+
         return answer;
     }
 }
