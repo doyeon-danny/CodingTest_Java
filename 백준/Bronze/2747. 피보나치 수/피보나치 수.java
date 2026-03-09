f524fa959bef82dca94dd945342c5c1149e6e28f
doyeon-danny <60081287+doyeon-danny@users.noreply.github.com>
2026-03-10 05:39:05 +0900

    [Bronze II] Title: 피보나치 수, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Integer> arr = new ArrayList<>();
+		arr.add(0);
+		arr.add(1);
+		for (int i = 2; i <= n; i++) {
+			arr.add(arr.get(i-1) + arr.get(i-2));
+		}
+		System.out.println(arr.get(n));
+	}
+
+}
